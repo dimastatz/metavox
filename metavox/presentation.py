aeca08d4b623f00815c3d8f8aaccb204121a7ca5
dimastatz <dima.statz@gmail.com>
2025-03-25 08:43:18 +0200

    read slides

---
+++ metavox/presentation.py
@@ -0,0 +1,11 @@
+"""Document class for handling the document file"""
+
+from io import BytesIO
+from pptx import Presentation
+
+
+def read_presentation(file_path) -> Presentation:
+    """Read the presentation from the document file"""
+    with open(file_path, "rb") as file:
+        source_stream = BytesIO(file.read())
+        return Presentation(source_stream)
--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,8 +1,24 @@
 """ test main flows of metavox"""
 
+import os
 import metavox
+import metavox.presentation as pt
+
+
+def get_resource_path(name: str, extension: str) -> str:
+    "get resources path"
+    current_path = os.path.dirname(__file__)
+    path = os.path.join(current_path, f"./resources/{name}")
+    return f"{path}.{extension}"
 
 
 def test_version():
     """test version"""
     assert metavox.__version__ == "0.1.0"
+
+
+def test_read_presentation():
+    """test presentation load"""
+    presentation = pt.read_presentation(get_resource_path("dummy_presentation", "pptx"))
+    assert presentation is not None
+    assert len(presentation.slides) == 4
