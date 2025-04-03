c86fcf379f81299a56de967823d7866bcd7da91c
Dima Statz <dima.statz@gmail.com>
2025-04-03 06:05:12 +0000

    update prepare image script

--- update_image.sh
+++ update_image.sh
@@ -1,7 +1,9 @@
-$!/bin/bash
+#!/bin/bash
 
-echo 'start'
+echo ' ===> start '
 sudo apt-get update
 sudo apt-get install default-jdk
 sudo apt-get install -y --no-install-recommends libreoffice libfontconfig1 libfreetype6 libx11-6 libxrender1 libxrender1 libxrender1 libxext6 libxcb1 libxcb1 libuuid1
 sudo rm -rf /var/lib/apt/lists/*
+
+echo ' ===> end '
