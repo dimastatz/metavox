48c5830d977fec15f33a24603574df4bed42e240
Dima Statz <dima.statz@gmail.com>
2025-05-29 05:29:00 +0000

    Update files

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -17,7 +17,7 @@ def get_resource_path(name: str, extension: str) -> str:
 
 def test_version():
     """test version"""
-    assert metavox.__version__ == "0.1.0"
+    assert metavox.__version__ == "0.1.1"
 
 
 def test_read_presentation():
