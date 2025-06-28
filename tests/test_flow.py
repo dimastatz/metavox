6c84ee38689eae50fccce55621e7459154c6dc81
Dima Statz <dima.statz@gmail.com>
2025-06-28 04:14:22 +0000

    add device

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -65,7 +65,13 @@ def test_libreoffice_version():
 
 def test_chatterbox():
     """test chatterbox"""
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+
     model = ChatterboxTTS.from_pretrained(device=device)
 
     text = "Text is nice, but audio is better."
