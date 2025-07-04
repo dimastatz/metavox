ddb66573c4e93bd6a9d893ea72a72d72539d26ca
Dima Statz <dima.statz@gmail.com>
2025-07-04 04:57:44 +0000

    add main function

---
+++ metavox/app.py
@@ -0,0 +1,10 @@
+"""Entry point for the Metavox application"""
+
+
+def main():
+    """Main function to run the Metavox application"""
+    print("Hello from main!")
+
+
+if __name__ == "__main__":
+    main()
