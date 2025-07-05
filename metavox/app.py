3ebdd36ae202693b8129932e00252a3d85dab779
Dima Statz <dima.statz@gmail.com>
2025-07-05 04:24:27 +0000

    add metavox

--- metavox/app.py
+++ metavox/app.py
@@ -1,9 +1,15 @@
 """Entry point for the Metavox application"""
 
+import logging as log
+import metavox.presentation as pt
+
+
+log.basicConfig(level=log.INFO)
+
 
 def main():
     """Main function to run the Metavox application"""
-    print("Hello from main!")
+    log.info("Starting Metavox application...")
 
 
 if __name__ == "__main__":
