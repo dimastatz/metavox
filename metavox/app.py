daf09df2a516c153b769540a04e1df90bd191ceb
Dima Statz <dima.statz@gmail.com>
2025-07-05 04:32:47 +0000

    main

--- metavox/app.py
+++ metavox/app.py
@@ -11,13 +11,16 @@ log.basicConfig(level=log.INFO)
 def main():
     """Main function to run the Metavox application"""
     log.info("Starting Metavox application...")
-    
+
     if len(sys.argv) < 2:
         log.error("Usage: python app.py <presentation_file>")
         sys.exit(1)
 
-    
-    pt.read_presentation("dummy_presentation.pptx")
+    # read presentation file
+    presentation = pt.read_presentation(sys.argv[1])
+    for i, _ in enumerate(presentation.slides):
+        notes = pt.get_speaker_notes(presentation, i)
+        log.info(f"Speaker Notes: {i} {notes}")
 
 
 if __name__ == "__main__":
