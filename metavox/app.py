95d7aedf94ce2ffcb44611e2cffba8bf6b977467
Dima Statz <dima.statz@gmail.com>
2025-07-05 04:28:17 +0000

    add main flow

--- metavox/app.py
+++ metavox/app.py
@@ -1,5 +1,6 @@
 """Entry point for the Metavox application"""
 
+import sys
 import logging as log
 import metavox.presentation as pt
 
@@ -10,6 +11,13 @@ log.basicConfig(level=log.INFO)
 def main():
     """Main function to run the Metavox application"""
     log.info("Starting Metavox application...")
+    
+    if len(sys.argv) < 2:
+        log.error("Usage: python app.py <presentation_file>")
+        sys.exit(1)
+
+    
+    pt.read_presentation("dummy_presentation.pptx")
 
 
 if __name__ == "__main__":
